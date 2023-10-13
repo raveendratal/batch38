5b1d698466fba9ecea57c035eb007f32755a9c64
raveendratal <raveendratal>
2023-10-13 02:40:10 +0000

    added ddl for customers

--- project_a/sample_notebook.sql
+++ project_a/sample_notebook.sql
@@ -4,3 +4,7 @@ select current_catalog(),current_database(),current_date(),current_timestamp()
 -- COMMAND ----------
 
 select current_date()
+
+-- COMMAND ----------
+
+create table customers(id int,name string)
